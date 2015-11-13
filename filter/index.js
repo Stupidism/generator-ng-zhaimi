11236c3dc4157903f8c516389653f128510251ff
stupidisum <sunfeng32@qq.com>
2015-11-13 21:05:12 +0800

    fix(filter): fileName missed

--- filter/index.js
+++ filter/index.js
@@ -23,11 +23,17 @@ Generator.prototype.prompting = function askFor() {
     name: 'dir',
     message: 'Where would you like to create this filter?',
     default: path.join(config.get('filterDirectory'), self.underscoredName),
+  }, {
+    name: 'fileName',
+    message: 'What file name would you like to use?',
+    default: config.get('defaultFileName') || 'filter',
+    when: function() {return config.get('fileNamePrompt');}
   }];
 
   this.prompt(prompts, function (props) {
     self.scriptAppName = props.moduleName || self.scriptAppName;
     self.dir = props.dir;
+    self.fileName = props.fileName;
     done();
   });
 };
